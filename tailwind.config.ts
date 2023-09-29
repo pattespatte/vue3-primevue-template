cde4409673b659b3fd145d8dd0ac7bbfbc607b4b
T1GIT <derbindima5@gmail.com>
2023-09-29 12:52:24 +0300

    linted

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        main: 'var(--font-family)',
+      },
       colors: {
         primary: 'var(--primary-color)',
       },
